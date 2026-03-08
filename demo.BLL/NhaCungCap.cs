a2e8e2ed7bdf4853fc20655020bfb56c396e1bdd
PhamVanPhuc <vanphuc2410.github@gmail.com>
2026-03-08 12:49:03 +0700

    Add project files.

---
+++ demo.BLL/NhaCungCap.cs
@@ -0,0 +1,22 @@
+﻿using demo.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.BLL
+{
+    public class NhaCungCap
+    {
+        private readonly CUA_HANG_TIEN_LOI_Entities db;
+
+        NhaCungCap()
+        {
+            db = new CUA_HANG_TIEN_LOI_Entities();
+        }
+
+
+
+    }
+}
