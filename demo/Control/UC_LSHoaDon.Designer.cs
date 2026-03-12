3d3c0a50d568924eb087b96108d7580f79180bc1
kietzuki <huynhkietzuki@gmail.com>
2026-03-12 23:24:17 +0700

    update 12/3 11:24

--- demo/Control/UC_LSHoaDon.Designer.cs
+++ demo/Control/UC_LSHoaDon.Designer.cs
@@ -29,28 +29,26 @@
         private void InitializeComponent()
         {
             this.panelHeader = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_XoaLoc = new System.Windows.Forms.Button();
+            this.btn_Loc = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.dtTuNgay = new System.Windows.Forms.DateTimePicker();
             this.dtDenNgay = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.cbNhanVien = new System.Windows.Forms.ComboBox();
             this.cbKhachHang = new System.Windows.Forms.ComboBox();
             this.cbCaLam = new System.Windows.Forms.ComboBox();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            this.btn_Loc = new System.Windows.Forms.Button();
-            this.btn_XoaLoc = new System.Windows.Forms.Button();
             this.panelTable = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
             this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
             this.panelHeader.SuspendLayout();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -60,33 +58,13 @@
             // 
             // panelHeader
             // 
-            this.panelHeader.BackColor = System.Drawing.Color.White;
-            this.panelHeader.Controls.Add(this.button2);
-            this.panelHeader.Controls.Add(this.button1);
+            this.panelHeader.BackColor = System.Drawing.Color.Silver;
             this.panelHeader.Controls.Add(this.label1);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
             this.panelHeader.Size = new System.Drawing.Size(1130, 81);
             this.panelHeader.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(978, 26);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 32);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Export PDF";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(833, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Export Excel";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -100,7 +78,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.Silver;
             this.panel1.Controls.Add(this.btn_XoaLoc);
             this.panel1.Controls.Add(this.btn_Loc);
             this.panel1.Controls.Add(this.tableLayoutPanel1);
@@ -109,6 +87,26 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1130, 115);
             this.panel1.TabIndex = 1;
+            // 
+            // btn_XoaLoc
+            // 
+            this.btn_XoaLoc.Location = new System.Drawing.Point(1022, 84);
+            this.btn_XoaLoc.Name = "btn_XoaLoc";
+            this.btn_XoaLoc.Size = new System.Drawing.Size(75, 23);
+            this.btn_XoaLoc.TabIndex = 2;
+            this.btn_XoaLoc.Text = "Xóa lọc";
+            this.btn_XoaLoc.UseVisualStyleBackColor = true;
+            this.btn_XoaLoc.Click += new System.EventHandler(this.btn_XoaLoc_Click);
+            // 
+            // btn_Loc
+            // 
+            this.btn_Loc.Location = new System.Drawing.Point(922, 84);
+            this.btn_Loc.Name = "btn_Loc";
+            this.btn_Loc.Size = new System.Drawing.Size(75, 23);
+            this.btn_Loc.TabIndex = 1;
+            this.btn_Loc.Text = "Lọc";
+            this.btn_Loc.UseVisualStyleBackColor = true;
+            this.btn_Loc.Click += new System.EventHandler(this.btn_Loc_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -142,6 +140,56 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1130, 76);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(920, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Tìm mã hóa đơn";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(712, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ca làm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(504, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Khách hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(296, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nhân viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(151, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Đến ngày";
+            // 
             // dtTuNgay
             // 
             this.dtTuNgay.Location = new System.Drawing.Point(6, 42);
@@ -165,56 +213,6 @@
             this.label2.Size = new System.Drawing.Size(68, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Từ ngày";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(151, 3);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Đến ngày";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(296, 3);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(83, 20);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Nhân viên";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(504, 3);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 20);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Khách hàng";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(712, 3);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(62, 20);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Ca làm";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(920, 3);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 20);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Tìm mã hóa đơn";
             // 
             // cbNhanVien
             // 
@@ -246,28 +244,11 @@
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(175, 22);
             this.txtSearch.TabIndex = 11;
-            // 
-            // btn_Loc
-            // 
-            this.btn_Loc.Location = new System.Drawing.Point(922, 84);
-            this.btn_Loc.Name = "btn_Loc";
-            this.btn_Loc.Size = new System.Drawing.Size(75, 23);
-            this.btn_Loc.TabIndex = 1;
-            this.btn_Loc.Text = "Lọc";
-            this.btn_Loc.UseVisualStyleBackColor = true;
-            // 
-            // btn_XoaLoc
-            // 
-            this.btn_XoaLoc.Location = new System.Drawing.Point(1022, 84);
-            this.btn_XoaLoc.Name = "btn_XoaLoc";
-            this.btn_XoaLoc.Size = new System.Drawing.Size(75, 23);
-            this.btn_XoaLoc.TabIndex = 2;
-            this.btn_XoaLoc.Text = "Xóa lọc";
-            this.btn_XoaLoc.UseVisualStyleBackColor = true;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
             // 
             // panelTable
             // 
-            this.panelTable.BackColor = System.Drawing.Color.White;
+            this.panelTable.BackColor = System.Drawing.Color.Silver;
             this.panelTable.Controls.Add(this.dgvHoaDon);
             this.panelTable.Controls.Add(this.label8);
             this.panelTable.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -275,16 +256,6 @@
             this.panelTable.Name = "panelTable";
             this.panelTable.Size = new System.Drawing.Size(1130, 391);
             this.panelTable.TabIndex = 2;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(6, 6);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(154, 20);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "Danh sách hóa đơn";
             // 
             // dgvHoaDon
             // 
@@ -296,6 +267,16 @@
             this.dgvHoaDon.RowTemplate.Height = 24;
             this.dgvHoaDon.Size = new System.Drawing.Size(1106, 347);
             this.dgvHoaDon.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(6, 6);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 20);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Danh sách hóa đơn";
             // 
             // UC_LSHoaDon
             // 
@@ -322,8 +303,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panelHeader;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
--- demo/Control/UC_LSHoaDon.cs
+++ demo/Control/UC_LSHoaDon.cs
@@ -44,5 +44,69 @@ namespace demo.Control
         {
             LoadHoaDon();
         }
+
+        private void btn_Loc_Click(object sender, EventArgs e)
+        {
+            DateTime tuNgay = dtTuNgay.Value;
+            DateTime denNgay = dtDenNgay.Value;
+
+            var data = db.HOA_DON
+                .Where(hd => hd.NgayLap >= tuNgay && hd.NgayLap <= denNgay)
+                .Select(hd => new
+                {
+                    hd.MaHoaDon,
+                    hd.NgayLap,
+                    SoLuongSP = db.CHI_TIET_HOA_DON
+                        .Where(ct => ct.MaHoaDon == hd.MaHoaDon)
+                        .Sum(ct => ct.SoLuong),
+
+                    TongTien = db.CHI_TIET_HOA_DON
+                        .Where(ct => ct.MaHoaDon == hd.MaHoaDon)
+                        .Sum(ct => ct.ThanhTien)
+                })
+                .ToList();
+
+            dgvHoaDon.DataSource = data;
+        }
+
+        private void btn_XoaLoc_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            cbNhanVien.SelectedIndex = -1;
+            cbKhachHang.SelectedIndex = -1;
+
+            dtTuNgay.Value = DateTime.Now.AddMonths(-1);
+            dtDenNgay.Value = DateTime.Now;
+
+            LoadHoaDon();
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+
+                string keyword = txtSearch.Text;
+
+                var data = db.HOA_DON
+                    .Where(hd => hd.MaHoaDon.ToString().Contains(keyword))
+                    .Select(hd => new
+                    {
+                        hd.MaHoaDon,
+                        hd.NgayLap,
+                        SoLuongSP = db.CHI_TIET_HOA_DON
+                            .Where(ct => ct.MaHoaDon == hd.MaHoaDon)
+                            .Sum(ct => ct.SoLuong),
+
+                        TongTien = db.CHI_TIET_HOA_DON
+                            .Where(ct => ct.MaHoaDon == hd.MaHoaDon)
+                            .Sum(ct => ct.ThanhTien)
+                    })
+                    .ToList();
+
+                dgvHoaDon.DataSource = data;
+            }
+        }
     }
 }
