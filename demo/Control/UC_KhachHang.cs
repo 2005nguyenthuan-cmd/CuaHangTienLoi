c27f9f1b43c20a70dd615685f5fd343d063c4c8b
PhanDinhAnh <dinhanhleo10@gmail.com>
2026-03-13 22:00:20 +0700

    10h133

--- demo/Control/UC_KhachHang.cs
+++ demo/Control/UC_KhachHang.cs
@@ -14,6 +14,30 @@ namespace demo.Control
 {
     public partial class UC_KhachHang : UserControl
     {
+        private Panel pnHeader;
+        private Label label2;
+        private Label label1;
+        private Panel panelStats;
+        private Panel panel2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Panel panel4;
+        private PictureBox pictureBox4;
+        private Label label9;
+        private Label label10;
+        private Panel panel3;
+        private PictureBox pictureBox3;
+        private Label label7;
+        private Label label8;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private Label label5;
+        private Label label6;
+        private Panel panel5;
+        private TextBox textBox1;
+        private PictureBox pictureBox5;
+        private Button btnThemKH;
         CUA_HANG_TIEN_LOI_Entities db = new CUA_HANG_TIEN_LOI_Entities();
         public UC_KhachHang()
         {
@@ -41,6 +65,337 @@ namespace demo.Control
                              .ToList();
 
             dgvKhachHang.DataSource = topKhach;
+        }
+
+        private void InitializeComponent()
+        {
+            this.pnHeader = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelStats = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnThemKH = new System.Windows.Forms.Button();
+            this.pnHeader.SuspendLayout();
+            this.panelStats.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnHeader
+            // 
+            this.pnHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pnHeader.Controls.Add(this.label2);
+            this.pnHeader.Controls.Add(this.label1);
+            this.pnHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnHeader.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.pnHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnHeader.Name = "pnHeader";
+            this.pnHeader.Size = new System.Drawing.Size(1200, 80);
+            this.pnHeader.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(25, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chăm sóc khách hàng và chương trình thành viên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(18, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý khách hàng ❤";
+            // 
+            // panelStats
+            // 
+            this.panelStats.BackColor = System.Drawing.Color.Transparent;
+            this.panelStats.Controls.Add(this.panel4);
+            this.panelStats.Controls.Add(this.panel3);
+            this.panelStats.Controls.Add(this.panel1);
+            this.panelStats.Controls.Add(this.panel2);
+            this.panelStats.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelStats.Location = new System.Drawing.Point(0, 80);
+            this.panelStats.Name = "panelStats";
+            this.panelStats.Size = new System.Drawing.Size(1200, 120);
+            this.panelStats.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(20, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 90);
+            this.panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(15, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tổng khách hàng";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(12, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 41);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "3";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(308, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 90);
+            this.panel1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label5.Location = new System.Drawing.Point(12, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 41);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "4350";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(15, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 23);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Tổng Điểm";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Location = new System.Drawing.Point(595, 15);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(250, 90);
+            this.panel3.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.ForeColor = System.Drawing.Color.DeepPink;
+            this.label7.Location = new System.Drawing.Point(12, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 41);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "14500K";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(15, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 23);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Chi TB/KH";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Location = new System.Drawing.Point(880, 15);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(250, 90);
+            this.panel4.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(12, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 41);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label10.Location = new System.Drawing.Point(15, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 23);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "VIP";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Controls.Add(this.btnThemKH);
+            this.panel5.Controls.Add(this.pictureBox5);
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 200);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1200, 90);
+            this.panel5.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBox1.Location = new System.Drawing.Point(74, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Tìm kiếm theo tên hoặc số điện thoại...";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::demo.Properties.Resources.xmag_search_find_export_locate_5984;
+            this.pictureBox5.Location = new System.Drawing.Point(40, 30);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::demo.Properties.Resources._12200crown_109565;
+            this.pictureBox4.Location = new System.Drawing.Point(189, 38);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::demo.Properties.Resources._269561_upward_trend_icon;
+            this.pictureBox3.Location = new System.Drawing.Point(189, 38);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::demo.Properties.Resources.star_favourite_15499;
+            this.pictureBox2.Location = new System.Drawing.Point(189, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::demo.Properties.Resources.systemusers_104569;
+            this.pictureBox1.Location = new System.Drawing.Point(189, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnThemKH
+            // 
+            this.btnThemKH.BackColor = System.Drawing.Color.HotPink;
+            this.btnThemKH.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThemKH.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThemKH.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnThemKH.Location = new System.Drawing.Point(529, 25);
+            this.btnThemKH.Name = "btnThemKH";
+            this.btnThemKH.Size = new System.Drawing.Size(200, 40);
+            this.btnThemKH.TabIndex = 4;
+            this.btnThemKH.Text = "+ Thêm khách hàng";
+            this.btnThemKH.UseVisualStyleBackColor = false;
+            // 
+            // UC_KhachHang
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panelStats);
+            this.Controls.Add(this.pnHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Name = "UC_KhachHang";
+            this.Size = new System.Drawing.Size(1200, 700);
+            this.pnHeader.ResumeLayout(false);
+            this.pnHeader.PerformLayout();
+            this.panelStats.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- demo/Menu.cs
+++ demo/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using demo.BLL.Service;
+using demo.Control;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -63,7 +64,10 @@ namespace demo
 
         private void btn_kh_Click(object sender, EventArgs e)
         {
-
+            UC_KhachHang control = new UC_KhachHang();
+            panel_Main.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            panel_Main.Controls.Add(control);
         }
 
         private void btn_ncc_Click(object sender, EventArgs e)
